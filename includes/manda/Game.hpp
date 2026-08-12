f996b1948912c010e789a635ff7f7964b584bea5
Sacha Barthonet <sacha.barthonet@gmail.com>
2026-08-12 18:52:00 +0200

    FIX: bad merge

---
+++ includes/manda/Game.hpp
@@ -0,0 +1,27 @@
+#ifndef MANDA_GAME_HPP
+#define MANDA_GAME_HPP
+
+#include "Board.hpp"
+#include "Move.hpp"
+
+class Game {
+
+    private:
+        Board _board;
+        Piece _currentPlayer;
+    
+    public:
+        Game() : _board(), _currentPlayer(BLACK) {};
+        ~Game() {};
+
+        void setCurrentPlayer(Piece player) { _currentPlayer = player; }
+
+        Piece opponent() const {
+            return _currentPlayer == BLACK ? WHITE : BLACK;
+        }
+
+        Piece getCurrentPlayer() const { return _currentPlayer; }
+        Board& getBoard() { return _board; }
+};
+
+#endif
